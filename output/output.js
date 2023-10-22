3249e162f187fe81eacd701686470792b3c58b63
gogofishman <gogofishman@qq.com>
2023-10-22 17:54:00 +0800

    Update output.js

--- output/output.js
+++ output/output.js
@@ -2,7 +2,7 @@
 // @name         bilbili界面美化
 // @description  让我们给B站界面变得现代一些吧!
 // @namespace    none
-// @version      1.1.6
+// @version      1.1.7
 // @author       gogofishman
 // @license      MIT
 // @match        *://*.bilibili.com/video/*
